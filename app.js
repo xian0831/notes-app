e1c1139053ea71d32236e15cb8c98a5a294530fc
andrew_zheng <xian0831@gmail.com>
2019-04-20 23:00:41 -0400

    add body options

--- app.js
+++ app.js
@@ -11,10 +11,16 @@ yargs.command({
             describe: 'Note title',
             demandOption: true,
             type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
         }
     },
     handler: (argv) => {
-        console.log('Adding a new note!', argv)
+        console.log(`Title: ${argv.title}`);
+        console.log(`Body: ${argv.body}`);
     }
 });
 
